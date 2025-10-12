f663e99c5dafead131280fa77b84244d2a92f90f
yogeshsingh-1 <yogesh.singh@mygstcafe.in>
2025-10-12 21:54:29 +0530

    Add readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- For continue watch our .ts code and changes to in .js-->
+npm run watch
+
+<!-- for running server -->
+npm run dev
+npm run server
+npm start
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const port = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+    return res.send("Hey it is work");
+});
+app.listen(port, () => console.log("Server is running on ", port));
