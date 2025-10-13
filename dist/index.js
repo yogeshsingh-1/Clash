13d52875473c3e16d2c01db80327c3efe8748d12
yogeshsingh-1 <yogesh.singh@mygstcafe.in>
2025-10-13 18:42:56 +0530

    added changes

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const port = process.env.PORT || 7000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-    return res.send("Hey it is work");
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./src/views"));
+app.get("/render", (req, res) => {
+    return res.render("welcome");
 });
-app.listen(port, () => console.log("Server is running on ", port));
+app.listen(port, () => console.log(`server is running on ${port}`));
--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app: Application = express();
 const port = process.env.PORT || 7000;
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./src/views"));
 
-app.get("/", (req: Request, res: Response) => {
-  
-  return res.send("Hey it is work");
+app.get("/render", (req: Request, res: Response) => {
+  return res.render("welcome");
 });
-app.listen(port, () => console.log("Server is running on ", port));
+app.listen(port, () => console.log(`server is running on ${port}`));
